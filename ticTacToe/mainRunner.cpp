80925d158422a4e479eb8eea2ff51bd41e8527f4
JasonFevang <jasonfevang@gmail.com>
2016-05-22 08:25:11 -0300

    Hello World! program for testing

---
+++ ticTacToe/mainRunner.cpp
@@ -0,0 +1,11 @@
+//This is a test file to make sure that we are able to collaborate changes with the code.
+//Pull this file, make a small change to it, make a commit, then push your changes
+//to the repo. I'll pull it, then try and run it as well.
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "hello world!\n";
+	system("pause");//Prompts for "press any key to continue..."
+	return 0;
+}
