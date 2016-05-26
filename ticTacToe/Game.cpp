2f6b54a372f8b6f03255c3c034d7b0e994c3810f
JasonFevang <jasonfevang@gmail.com>
2016-05-26 18:13:29 -0300

    win-check tests complete and Game.winner() refactored

--- ticTacToe/Game.cpp
+++ ticTacToe/Game.cpp
@@ -19,33 +19,23 @@ Player* Game::getPlayer() {
 
 //returns the marker of who won the game that turn. Empty means no one
 marker Game::winner() {
-	///horizontal///
-	//check top horizontal
-	if ((board->getSpace(0) != empt && board->getSpace(0) == board->getSpace(1) && board->getSpace(0) == board->getSpace(2)))
+	if (checkThree(topHoriz) || checkThree(leftVert)) {
 		return board->getSpace(0);
-	//check middle horizontal
-	if ((board->getSpace(3) != empt && board->getSpace(3) == board->getSpace(4) && board->getSpace(3) == board->getSpace(5)))
-		return board->getSpace(3);
-	//check bottom horizontal
-	if ((board->getSpace(6) != empt && board->getSpace(6) == board->getSpace(7) && board->getSpace(6) == board->getSpace(8)))
-		return board->getSpace(6);
+	}
+	else if (checkThree(midHoriz) || checkThree(midVert) || checkThree(upDiag) || checkThree(downDiag)) {
+		return board->getSpace(4);
+	}
+	else if (checkThree(botHoriz) || checkThree(rightVert)) {
+		return board->getSpace(8);
+	}
+	else return empt;
+}
 
-	///vertical///
-	//check left vertical
-	if ((board->getSpace(0) != empt && board->getSpace(0) == board->getSpace(3) && board->getSpace(0) == board->getSpace(6)))
-		return board->getSpace(0);
-	//check middle vertical
-	if ((board->getSpace(1) != empt && board->getSpace(1) == board->getSpace(4) && board->getSpace(1) == board->getSpace(7)))
-		return board->getSpace(1);
-	//check right vertical
-	if ((board->getSpace(2) != empt && board->getSpace(2) == board->getSpace(5) && board->getSpace(2) == board->getSpace(8)))
-		return board->getSpace(2);
-
-	///check diagonals////
-	//check forward slash vertical
-	if (board->getSpace(0) == board->getSpace(4) && board->getSpace(0) == board->getSpace(8))
-		return board->getSpace(1);
-	//check right vertical
-	if (board->getSpace(2) == board->getSpace(5) && board->getSpace(2) == board->getSpace(8))
-		return board->getSpace(2);
+bool Game::checkThree(const int boardIndexes[]) {
+	if (board->getSpace(boardIndexes[0]) != empt && board->getSpace(boardIndexes[1]) != empt && board->getSpace(boardIndexes[2]) != empt) {
+		if ((board->getSpace(boardIndexes[0]) == board->getSpace(boardIndexes[1])) && (board->getSpace(boardIndexes[0]) == board->getSpace(boardIndexes[2]))) {
+			return true;
+		}
+	}
+	else return false;
 }
--- ticTacToe/Game.h
+++ ticTacToe/Game.h
@@ -3,12 +3,25 @@
 
 #include "All.h"
 
+const int topHoriz[3] = { 0, 1, 2 };
+const int midHoriz[3] = { 3, 4, 5 };
+const int botHoriz[3] = { 6, 7, 8 };
+
+const int leftVert[3] = { 0, 3, 6 };
+const int midVert[3] = { 1, 4, 7 };
+const int rightVert[3] = { 2, 5, 8 };
+
+const int downDiag[3] = { 0, 4, 8 };
+const int upDiag[3] = { 6, 4, 2 };
+
 class Game
 {
 private:
 	Board *board; //pointer to the board being played on
 	Player *player; //pointer to the player
 	bool playerFirst; //True means player goes first and is crosses
+
+	bool checkThree(const int[]); //Checks three squares on the board to be equal and not empt
 public:
 	Game();//public constructor, instantiates board and player
 
